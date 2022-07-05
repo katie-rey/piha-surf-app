a0c24f42396f29c86355d55f828066a937f8617c
Katie-Rey <katiejorey@gmail.com>
2022-07-05 18:09:43 +1200

    setup initial API call

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-
 import { fetchFruits } from '../actions'
+import Home from './Home'
 
-function App () {
-  const fruits = useSelector(state => state.fruits)
+function App() {
+  const fruits = useSelector((state) => state.fruits)
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(fetchFruits())
@@ -12,14 +12,7 @@ function App () {
 
   return (
     <>
-      <div className='app'>
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>
-          {fruits.map(fruit => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
-      </div>
+      <Home />
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const path = require('path')
-
+const request = require('superagent')
+const cors = require('cors')
 const fruitRoutes = require('./routes/fruits')
+const axios = require('axios')
+require('dotenv').config()
 
 const server = express()
 
@@ -9,5 +12,37 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/fruits', fruitRoutes)
+
+server.get('/weather', (req, res) => {
+  const options = {
+    method: 'GET',
+    url: process.env.WEATHER_URL,
+    params: {
+      q: '-36.9545, 174.4744',
+      key: process.env.WEATHER_KEY,
+      format: 'json',
+      tp: '24',
+    },
+  }
+
+  axios
+    .request(options)
+    .then((response) => {
+      res.json(response.data)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+})
+
+//   request
+//     .get('https://random-d.uk/api/random')
+//     .set('accept', 'application/json')
+//     .then((response) => {
+//       console.log('this is the server speaking', response.body)
+//       res.json(response.body)
+//     })
+//     .catch((err) => console.error(err))
+// })
 
 module.exports = server
